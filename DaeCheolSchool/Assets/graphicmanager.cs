08774cb2d91513090b21432277a4c22a0b19a178
PlayerStudio1 <inamanag@naver.com>
2021-09-01 18:41:51 +0900

    그래픽세이브

---
+++ DaeCheolSchool/Assets/graphicmanager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class graphicmanager : MonoBehaviour
+{
+    public GameObject cubeppp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startscreen.graphicset == 1)
+        {
+            cubeppp.SetActive(true);
+        } 
+        else
+        {
+            cubeppp.SetActive(false);
+        }
+    }
+}
