886ab490f0b323e0898ea593bb6ef121891d9e60
PlayerStudio1 <inamanag@naver.com>
2021-09-04 15:31:36 +0900

    코딩 거지같네

--- DaeCheolSchool/Assets/scripts/Mouse.cs
+++ DaeCheolSchool/Assets/scripts/Mouse.cs
@@ -4,25 +4,75 @@ using UnityEngine;
 
 public class Mouse : MonoBehaviour
 {
-    public bool mobilesupport;
+    public bool mobilesupport = false;
     public static float mouseSpeed = 5f;
     public Transform playerBody;
+    public Camera cam;
+    private Touch initTouch = new Touch();
     float xRotation = 0f;
+
+    private float rotx = 0f;
+    private float roty = 0f;
+    private Vector3 origRot;
+
+    public float rotSpeed = 0.5f;
+    public float dir = -1;
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        switch(mobilesupport)
+        {
+            case false:
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                break;
+
+            case true:
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+                origRot = cam.transform.eulerAngles;
+                rotx = origRot.x;
+                roty = origRot.y;
+                break;
+        }
+        
     }
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed;
+        if (mobilesupport == false)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSpeed;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+        else
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    initTouch = touch;
+                }
+                else if (touch.phase == TouchPhase.Moved)
+                {
+                    float deltaX = initTouch.position.x - touch.position.x;
+                    float deltaY = initTouch.position.y - touch.position.y;
+                    rotx -= deltaY * Time.deltaTime * rotSpeed * dir;
+                    roty += deltaX * Time.deltaTime * rotSpeed * dir;
+                    rotx = Mathf.Clamp(rotx, -45f, 45f);
+                    cam.transform.eulerAngles = new Vector3(rotx, roty, 0f);
+                }
+                else if (touch.phase == TouchPhase.Ended)
+                {
+                    initTouch = new Touch();
+                }
+            }
+        }
     }
 }
---
+++ DaeCheolSchool/Assets/scripts/managejoystick.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class managejoystick : MonoBehaviour, IDragHandler
+{
+    public Image joystickbg;
+    public Image joystick;
+    private Vector2 posInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystickbg = GetComponent<Image>();
+        joystick = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            joystickbg.rectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out posInput))
+        {
+            posInput.x = posInput.x / (joystickbg.rectTransform.sizeDelta.x);
+            posInput.y = posInput.y / (joystickbg.rectTransform.sizeDelta.y);
+            Debug.Log(posInput.x.ToString() + "/" + posInput.y.ToString());
+        }
+
+        if (posInput.magnitude > 1.0f)
+        {
+            posInput = posInput.normalized;
+        }
+
+        joystick.rectTransform.anchoredPosition = new Vector2(
+            posInput.x * (joystickbg.rectTransform.sizeDelta.x / 4),
+            posInput.y * (joystickbg.rectTransform.sizeDelta.y / 4));
+    }
+}
