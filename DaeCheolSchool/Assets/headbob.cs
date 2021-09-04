2f89959d3096191516c6b11a074ade0ef3496ae1
PlayerStudio1 <inamanag@naver.com>
2021-09-04 11:02:31 +0900

    모션

--- DaeCheolSchool/Assets/headbob.cs
+++ DaeCheolSchool/Assets/headbob.cs
@@ -4,5 +4,64 @@ using UnityEngine;
 
 public class headbob : MonoBehaviour
 {
-   
+    public CharacterController playerController;
+    public Animation anim; //Empty GameObject's animation component
+    private bool isMoving;
+
+    private bool left;
+    private bool right;
+
+    void CameraAnimations()
+    {
+        if (playerController.isGrounded == true)
+        {
+            if (isMoving == true)
+            {
+                if (left == true)
+                {
+                    if (!anim.isPlaying)
+                    {//Waits until no animation is playing to play the next
+                        anim.Play("cameramove");
+                        left = false;
+                        right = true;
+                    }
+                }
+                if (right == true)
+                {
+                    if (!anim.isPlaying)
+                    {
+                        anim.Play("cameramove");
+                        right = false;
+                        left = true;
+                    }
+                }
+            }
+        }
+    }
+
+
+    void Start()
+    { //First step in a new scene/life/etc. will be "walkLeft"
+        left = true;
+        right = false;
+    }
+
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal"); //Keyboard input to determine if player is moving
+        float inputY = Input.GetAxis("Vertical");
+
+        if (inputX != 0 || inputY != 0)
+        {
+            isMoving = true;
+        }
+        else if (inputX == 0 && inputY == 0)
+        {
+            isMoving = false;
+        }
+
+        CameraAnimations();
+
+    }
 }
