abb96172a6987f7017d44bfc097a0b21ca2a4cab
PlayerStudio1 <inamanag@naver.com>
2021-10-18 19:42:41 +0900

    조각들 충돌 제거

---
+++ DaeCheolSchool/Assets/collisionsfix.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionsfix : MonoBehaviour
+{
+    public Transform DoorPrefab;
+
+    void Start()
+    {
+        Transform bullet = Instantiate(DoorPrefab) as Transform;
+        Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<Collider>());
+    }
+}
