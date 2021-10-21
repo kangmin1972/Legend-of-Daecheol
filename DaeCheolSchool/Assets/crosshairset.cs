a4bd750b98e461cbd66d2fe4db23292fc79feda7
PlayerStudio1 <inamanag@naver.com>
2021-10-21 19:12:13 +0900

    조준점 설정

---
+++ DaeCheolSchool/Assets/crosshairset.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class crosshairset : MonoBehaviour
+{
+    public GameObject crosshair;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startscreen.iscrosshairon == 1)
+        {
+            crosshair.SetActive(true);
+        }
+    }
+}
