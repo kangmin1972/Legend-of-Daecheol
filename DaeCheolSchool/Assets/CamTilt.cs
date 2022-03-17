a59a94507e91a2987bb0395f3ceebffe920d091d
PlayerStudio1 <inamanag@naver.com>
2022-03-17 21:46:20 +0900

    훨씬 나은 조작감 + 화면 기울임

---
+++ DaeCheolSchool/Assets/CamTilt.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamTilt : MonoBehaviour
+{
+
+    //Editor variables, you can customize these
+    public float _tiltAmount = 5;
+    public KeyCode _leftBtn = KeyCode.A; //A is default
+    public KeyCode _rightBtn = KeyCode.D; //D is default
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // If _leftBtn key is hit, rotate Z axis of camera by _tiltAmount
+        if (Input.GetKeyDown(_leftBtn))
+        {
+            transform.Rotate(0, 0, _tiltAmount);
+        }
+        else if (Input.GetKeyUp(_leftBtn))
+        {
+            transform.Rotate(0, 0, -_tiltAmount);
+        }
+
+        // Same as above, but inverted values
+        if (Input.GetKeyDown(_rightBtn))
+        {
+            transform.Rotate(0, 0, -_tiltAmount);
+        }
+        else if (Input.GetKeyUp(_rightBtn))
+        {
+            transform.Rotate(0, 0, _tiltAmount);
+        }
+    }
+}
