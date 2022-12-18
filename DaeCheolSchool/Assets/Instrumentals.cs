deacf65e4ce7ddbc8d09c5413606e7ac7514e5da
PlayerStudio1 <inamanag@naver.com>
2022-12-18 22:18:42 +0900

    많은 맵

---
+++ DaeCheolSchool/Assets/Instrumentals.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instrumentals : MonoBehaviour
+{
+    public AudioSource[] samples;
+    public int x;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "weapon")
+        {
+            if (usinghammer.ishammerused == true)
+            {
+                SamplePlay();
+            }
+        }
+
+        if (other.tag == "tennisbullet")
+        {
+            SamplePlay();
+        }
+    }
+
+    void SamplePlay()
+    {
+        x = Random.Range(0, samples.Length);
+        samples[x].Play();
+    }
+}
